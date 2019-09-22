d30d324a0e72752081beb969374b08fa6d3ca22a
oda <chunyex@gmail.com>
2019-09-22 16:42:20 +0900

    add search function

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,12 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
 end
